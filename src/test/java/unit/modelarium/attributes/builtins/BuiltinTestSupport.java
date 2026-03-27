99ac10f35043a2c241ee47eed958dcdde56904f0
JoshMcDonagh <joshua.mcdonagh.99@gmail.com>
2026-03-27 16:36:45 +0000

    Add start of unit tests for builtin functionality

---
+++ src/test/java/unit/modelarium/attributes/builtins/BuiltinTestSupport.java
@@ -0,0 +1,94 @@
+package unit.modelarium.attributes.builtins;
+
+import modelarium.ModelClock;
+import modelarium.ModelElement;
+import modelarium.ModelElementAccessor;
+import modelarium.attributes.AttributeSet;
+import modelarium.attributes.AttributeSetCollection;
+import modelarium.attributes.Properties;
+import modelarium.attributes.Property;
+import modelarium.attributes.functional.FunctionalProperty;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class BuiltinTestSupport {
+    private BuiltinTestSupport() {}
+
+    public static class TestModelElement extends ModelElement {
+        public TestModelElement(AttributeSetCollection attributeSetCollection) {
+            super("test-element", attributeSetCollection);
+        }
+
+        @Override
+        public void run() {
+            // no-op
+        }
+
+        @Override
+        public ModelElement deepCopy() {
+            return null;
+        }
+    }
+
+    public static TestModelElement elementWith(AttributeSet... attributeSets) {
+        AttributeSetCollection collection = new AttributeSetCollection();
+        for (AttributeSet attributeSet : attributeSets)
+            collection.add(attributeSet);
+        return new TestModelElement(collection);
+    }
+
+    public static AttributeSet attributeSet(String name, Property<?>... properties) {
+        Properties props = new Properties();
+        for (Property<?> property : properties)
+            props.add(property);
+        return new AttributeSet(
+                name,
+                new modelarium.attributes.Events(),
+                props,
+                new modelarium.attributes.Events()
+        );
+    }
+
+    public static FunctionalProperty<Double> mutableDoubleProperty(String name, double initialValue) {
+        FunctionalProperty<Double> property = new FunctionalProperty<>(
+                name,
+                true,
+                Double.class,
+                (element, value) -> value,
+                (element, currentValue, newValue) -> newValue,
+                (element, currentValue) -> currentValue
+        );
+        property.set(initialValue);
+        return property;
+    }
+
+    public static FunctionalProperty<Boolean> mutableBooleanProperty(String name, boolean initialValue) {
+        FunctionalProperty<Boolean> property = new FunctionalProperty<>(
+                name,
+                true,
+                Boolean.class,
+                (element, value) -> value,
+                (element, currentValue, newValue) -> newValue,
+                (element, currentValue) -> currentValue
+        );
+        property.set(initialValue);
+        return property;
+    }
+
+    public static void attachWithoutClock(TestModelElement element) {
+        ModelElementAccessor accessor = mock(ModelElementAccessor.class);
+        element.setModelElementAccessor(accessor);
+        element.getAttributeSetCollection().setAssociatedModelElement(element);
+    }
+
+    public static void attachClock(TestModelElement element, int tick) {
+        ModelElementAccessor accessor = mock(ModelElementAccessor.class);
+        ModelClock clock = mock(ModelClock.class);
+        when(accessor.getModelClock()).thenReturn(clock);
+        when(clock.getTick()).thenReturn(tick);
+
+        element.setModelElementAccessor(accessor);
+        element.getAttributeSetCollection().setAssociatedModelElement(element);
+    }
+}
---
+++ src/test/java/unit/modelarium/attributes/builtins/properties/BuiltinPropertyTest.java
@@ -0,0 +1,111 @@
+package unit.modelarium.attributes.builtins.properties;
+
+import modelarium.attributes.builtins.properties.AddDoubleProperty;
+import modelarium.attributes.builtins.properties.ClampDoubleProperty;
+import modelarium.attributes.builtins.properties.ConstantDoubleProperty;
+import modelarium.attributes.builtins.properties.CopyDoubleProperty;
+import modelarium.attributes.builtins.refs.LiteralDoubleRef;
+import org.junit.jupiter.api.Test;
+import unit.modelarium.attributes.builtins.BuiltinTestSupport;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuiltinPropertyTest {
+
+    @Test
+    void addDoublePropertyAddsResolvedDeltaOnRun() {
+        AddDoubleProperty property = new AddDoubleProperty("value", 10.0, new LiteralDoubleRef(2.5));
+        var element = BuiltinTestSupport.elementWith(
+                BuiltinTestSupport.attributeSet("core", property)
+        );
+        BuiltinTestSupport.attachWithoutClock(element);
+
+        property.run();
+
+        assertEquals(12.5, property.get());
+    }
+
+    @Test
+    void clampDoublePropertyClampsWithinBounds() {
+        ClampDoubleProperty property = new ClampDoubleProperty(
+                "value",
+                12.0,
+                new LiteralDoubleRef(0.0),
+                new LiteralDoubleRef(10.0)
+        );
+        var element = BuiltinTestSupport.elementWith(
+                BuiltinTestSupport.attributeSet("core", property)
+        );
+        BuiltinTestSupport.attachWithoutClock(element);
+
+        property.run();
+
+        assertEquals(10.0, property.get());
+    }
+
+    @Test
+    void clampDoublePropertyThrowsWhenMinGreaterThanMax() {
+        ClampDoubleProperty property = new ClampDoubleProperty(
+                "value",
+                5.0,
+                new LiteralDoubleRef(10.0),
+                new LiteralDoubleRef(0.0)
+        );
+        var element = BuiltinTestSupport.elementWith(
+                BuiltinTestSupport.attributeSet("core", property)
+        );
+        BuiltinTestSupport.attachWithoutClock(element);
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class, property::run);
+
+        assertEquals("ClampDoubleProperty min cannot be greater than max", exception.getMessage());
+    }
+
+    @Test
+    void constantDoublePropertyCannotBeSet() {
+        ConstantDoubleProperty property = new ConstantDoubleProperty("constant", 7.5);
+
+        UnsupportedOperationException exception =
+                assertThrows(UnsupportedOperationException.class, () -> property.set(3.0));
+
+        assertEquals("ConstantDoubleProperty cannot be modified after construction", exception.getMessage());
+        assertEquals(7.5, property.get());
+    }
+
+    @Test
+    void copyDoublePropertyReadsValueFromSourceProperty() {
+        var source = BuiltinTestSupport.mutableDoubleProperty("source", 42.0);
+        CopyDoubleProperty copy = new CopyDoubleProperty("copy", "sourceSet", "source");
+
+        var element = BuiltinTestSupport.elementWith(
+                BuiltinTestSupport.attributeSet("sourceSet", source),
+                BuiltinTestSupport.attributeSet("derivedSet", copy)
+        );
+        BuiltinTestSupport.attachWithoutClock(element);
+
+        assertEquals(42.0, copy.get());
+    }
+
+    @Test
+    void copyDoublePropertyThrowsWhenNotAssociatedWithElement() {
+        CopyDoubleProperty copy = new CopyDoubleProperty("sourceSet", "source");
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class, copy::get);
+
+        assertEquals("CopyDoubleProperty has no associated ModelElement", exception.getMessage());
+    }
+
+    @Test
+    void copyDoublePropertyThrowsWhenSourcePropertyMissing() {
+        CopyDoubleProperty copy = new CopyDoubleProperty("copy", "sourceSet", "missing");
+
+        var element = BuiltinTestSupport.elementWith(
+                BuiltinTestSupport.attributeSet("sourceSet")
+        );
+        BuiltinTestSupport.attachWithoutClock(element);
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class, copy::get);
+
+        assertEquals("Source property not found: missing in sourceSet", exception.getMessage());
+    }
+}
