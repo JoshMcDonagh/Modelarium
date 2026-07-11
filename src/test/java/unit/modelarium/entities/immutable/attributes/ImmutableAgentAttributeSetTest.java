379682cb5ffe46a2b321810a297bf184897b759b
JoshMcDonagh <jmmarland99@gmail.com>
2026-07-11 23:41:05 +0100

    Add further unit tests for ImmutableAgentAttributeSets

--- src/test/java/unit/modelarium/entities/immutable/attributes/ImmutableAgentAttributeSetTest.java
+++ src/test/java/unit/modelarium/entities/immutable/attributes/ImmutableAgentAttributeSetTest.java
@@ -1,16 +1,18 @@
 package unit.modelarium.entities.immutable.attributes;
 
 import modelarium.entities.attributes.AgentAttributeSet;
+import modelarium.entities.contexts.AgentSimulationContext;
 import modelarium.entities.immutable.attributes.ImmutableAgentAttributeSet;
 import modelarium.entities.immutable.attributes.ImmutableAttributeSet;
+import modelarium.entities.logging.AttributeSetLog;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import static unit.modelarium.entities.immutable.attributes.ImmutableAttributeSetTestHelpers.makeAttributeSet;
 import static unit.modelarium.entities.immutable.attributes.ImmutableAttributeSetTestHelpers.makeImmutableAttributeSet;
 
@@ -42,5 +44,50 @@ public class ImmutableAgentAttributeSetTest {
         );
 
         assertEquals(attributeSetName, immutableAttributeSet.name());
+    }
+
+    @Test
+    public void testGetLog() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        AttributeSetLog<?> attributeSetLog = mock(AttributeSetLog.class);
+        AgentAttributeSet attributeSet = spy(makeAttributeSet(
+                AgentAttributeSet.class,
+                "testAttributeSetName",
+                new ArrayList<>()
+        ));
+        doReturn(attributeSetLog).when(attributeSet).getLog();
+
+        ImmutableAgentAttributeSet immutableAgentAttributeSet = new ImmutableAgentAttributeSet(attributeSet);
+
+        assertSame(attributeSetLog, immutableAgentAttributeSet.getLog());
+    }
+
+    @Test
+    public void testGetEvent_WithIndex() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetEvent_WithName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetRoutine_WithIndex() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetRoutine_WithName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetProperty_WithIndex() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetProperty_WithName() {
+        fail("Not yet implemented");
     }
 }
