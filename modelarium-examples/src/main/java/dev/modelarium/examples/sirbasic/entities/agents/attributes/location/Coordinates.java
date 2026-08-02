15e15f2c5123fa3d0d669fd91425cd758045f939
JoshMcDonagh <jmmarland99@gmail.com>
2026-08-02 22:36:59 +0100

    Modify Coordinates toString method

--- modelarium-examples/src/main/java/dev/modelarium/examples/sirbasic/entities/agents/attributes/location/Coordinates.java
+++ modelarium-examples/src/main/java/dev/modelarium/examples/sirbasic/entities/agents/attributes/location/Coordinates.java
@@ -13,7 +13,7 @@ public class Coordinates {
 
     @Override
     public String toString() {
-        return "x: " + x + ", y: " + y;
+        return "x: " + x + " y: " + y;
     }
 
     public void setX(int x) {
